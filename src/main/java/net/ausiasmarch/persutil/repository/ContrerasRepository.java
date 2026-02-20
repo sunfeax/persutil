2fa971a363a338563c9b639df38a9a42a9d9f57f
Vladislav Uski <sunfeax@gmail.com>
2026-02-20 09:14:50 +0100

    Trim database.sql to uski-only schema

--- src/main/java/net/ausiasmarch/persutil/repository/ContrerasRepository.java
+++
@@ -1,9 +0,0 @@
-package net.ausiasmarch.persutil.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import net.ausiasmarch.persutil.entity.ContrerasEntity;
-
-public interface ContrerasRepository extends JpaRepository<ContrerasEntity, Long>{
-    
-}
